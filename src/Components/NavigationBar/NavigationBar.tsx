9d925587cd38ff502c7a789b86620ee4e9826c8c
Abdulelah Hajjar <abdulelahhajjar@gmail.com>
2021-04-08 23:00:11 +0300

    Add shadow to navigation bar; Extract inner UI components in NavigationBar.tsx for better readability

--- src/Components/NavigationBar/NavigationBar.tsx
+++ src/Components/NavigationBar/NavigationBar.tsx
@@ -14,37 +14,60 @@ function NavigationBar() {
 	const store = useContext(StoreContext);
 	const cart = useContext(CartContext);
 
-	return (
-		<Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+	const brand = () => {
+		return (
 			<Navbar.Brand href="#">
 				<img
 					alt={store.name}
 					src={layout.logo}
-					width="64"
-					height="64"
+					height="72"
 					className="d-inline-block align-top"
 				/>
-			</Navbar.Brand>{" "}
+			</Navbar.Brand>
+		);
+	};
+
+	const actions = () => {
+		return (
+			<Nav.Link>
+				{layout.navigation.actions.map((action) => {
+					return (
+						<Link to={{ pathname: action.value }}>
+							<Button>{action.text}</Button>
+						</Link>
+					);
+				})}
+			</Nav.Link>
+		);
+	};
+
+	const shoppingCart = () => {
+		return (
+			<Nav.Link>
+				<IconButton
+					icon={cartIcon}
+					badgeNumber={cart.numItems()}
+					alt={`Shopping Cart (Number of Items: ${cart.numItems()})`}
+				/>
+			</Nav.Link>
+		);
+	};
+
+	return (
+		<Navbar
+			collapseOnSelect
+			expand="sm"
+			bg="light"
+			variant="light"
+			sticky="top"
+			className={styles.navigation_bar}
+		>
+			{brand()}
 			<Navbar.Toggle aria-controls="responsive-navbar-nav" />
 			<Navbar.Collapse id="responsive-navbar-nav">
 				<Nav className={`mr-auto ${styles.actions_container}`}>
-					<Nav.Link>
-						{layout.navigation.actions.map((action) => {
-							return (
-								<Link to={{ pathname: action.value }}>
-									<Button>{action.text}</Button>
-								</Link>
-							);
-						})}
-					</Nav.Link>
-
-					<Nav.Link>
-						<IconButton
-							icon={cartIcon}
-							badgeNumber={cart.numItems()}
-							alt={`Shopping Cart (Number of Items: ${cart.numItems()})`}
-						/>
-					</Nav.Link>
+					{actions()}
+					{shoppingCart()}
 				</Nav>
 			</Navbar.Collapse>
 		</Navbar>
