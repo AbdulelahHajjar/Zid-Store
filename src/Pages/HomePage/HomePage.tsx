657354ce4ca227b084b2ee6574523c55d6d82f01
Abdulelah Hajjar <abdulelahhajjar@gmail.com>
2021-04-13 00:49:10 +0300

    Implement responsiveness in home page

--- src/Pages/HomePage/HomePage.tsx
+++ src/Pages/HomePage/HomePage.tsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import React, { useContext } from "react";
 import CoverImage from "Components/CoverImage/CoverImage";
 import LayoutContext from "Contexts/LayoutContext";
 import styles from "./HomePage.module.scss";
@@ -6,6 +6,7 @@ import Layout from "Models/Layout";
 import ProductsSection from "Components/ProductsSection/ProductsSection";
 import CustomButton from "Components/Button/CustomButton";
 import { Link } from "react-router-dom";
+import { Col, Container, Row } from "react-bootstrap";
 
 function HomePage() {
 	const layout: Layout = useContext<Layout>(LayoutContext);
@@ -17,20 +18,30 @@ function HomePage() {
 	};
 
 	return (
-		<div>
+		<React.Fragment>
 			<div className={styles.cover_image_wrapper}>
 				<CoverImage imageUrl={layout.body.cover_image} />
 			</div>
+			<Container fluid>
+				<Row>
+					<Col md={1} sm={0} />
+					<Col md={10} sm={12}>
+						<div className={styles.main_content}>{sections()}</div>
 
-			<div className={styles.main_content}>{sections()}</div>
+						<div className={styles.all_products_section}>
+							<div className={styles.largeTitle}>
+								أو تصفح جميع المنتجات!
+							</div>
+							<Link to="/products">
+								<CustomButton>جميع المنتجات</CustomButton>
+							</Link>
+						</div>
+					</Col>
 
-			<div className={styles.all_products_section}>
-				<div className={styles.largeTitle}>أو تصفح جميع المنتجات!</div>
-				<Link to="/products">
-					<CustomButton>جميع المنتجات</CustomButton>
-				</Link>
-			</div>
-		</div>
+					<Col md={1} sm={0} />
+				</Row>
+			</Container>
+		</React.Fragment>
 	);
 }
 
