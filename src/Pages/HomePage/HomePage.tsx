7c5e94d82392f6c42ae0a5955c01784665c77999
Abdulelah Hajjar <abdulelahhajjar@gmail.com>
2021-04-13 00:16:39 +0300

    Remove unused imports

--- src/Pages/HomePage/HomePage.tsx
+++ src/Pages/HomePage/HomePage.tsx
@@ -1,19 +1,13 @@
-import React, { useContext } from "react";
-import ProductCardView from "Components/ProductCardView/ProductCardView";
-import StoreContext from "Contexts/StoreContext";
+import { useContext } from "react";
 import CoverImage from "Components/CoverImage/CoverImage";
 import LayoutContext from "Contexts/LayoutContext";
-import HorizontalList from "Components/HorizontalList/HorizontalList";
 import styles from "./HomePage.module.scss";
-import EmojiLabel from "Components/EmojiLabel/EmojiLabel";
 import Layout from "Models/Layout";
 import ProductsSection from "Components/ProductsSection/ProductsSection";
 import CustomButton from "Components/Button/CustomButton";
 import { Link } from "react-router-dom";
-import { Button, Container, Jumbotron } from "react-bootstrap";
 
 function HomePage() {
-	const store = useContext(StoreContext);
 	const layout: Layout = useContext<Layout>(LayoutContext);
 
 	const sections = () => {
@@ -31,7 +25,7 @@ function HomePage() {
 			<div className={styles.main_content}>{sections()}</div>
 
 			<div className={styles.all_products_section}>
-				<div className={styles.largeTitle}>أو تصفح كل المنتجات!</div>
+				<div className={styles.largeTitle}>أو تصفح جميع المنتجات!</div>
 				<Link to="/products">
 					<CustomButton>جميع المنتجات</CustomButton>
 				</Link>
